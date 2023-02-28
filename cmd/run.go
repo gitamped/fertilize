d58ca47282090448db2c7646b8384f342b878c0f
gitamped <126345532+gitamped@users.noreply.github.com>
2023-02-27 19:20:55 -0500

    adds ability to pass cmdline args to program

--- cmd/run.go
+++ cmd/run.go
@@ -4,9 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/gitamped/fertilize/parser"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	outfile    string
+	pkgs       string
+	v          bool
+	ignoreList string
 )
 
 var rootCmd = &cobra.Command{
@@ -15,7 +24,11 @@ var rootCmd = &cobra.Command{
 	Long: `A way to generate package information to use in a template engine.
 		   Use to generate boilerplate code and documents.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		p := parser.New([]string{"github.com/gitamped/fertilize/testdata/services/pleasantries"}...)
+
+		patterns := strings.Split(viper.GetString("pkgs"), ",")
+		p := parser.New(patterns...)
+		p.ExcludeInterfaces = strings.Split(viper.GetString("ignore"), ",")
+		p.Verbose = viper.GetBool("verbose")
 		def, err := p.Parse()
 		if err != nil {
 			panic("err parsing")
@@ -30,4 +43,16 @@ func Execute() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&outfile, "out", "", "output file (default: stdout)")
+	rootCmd.PersistentFlags().StringVar(&pkgs, "pkgs", "./...", "comma separated list of package patterns (default: ./...).")
+	rootCmd.PersistentFlags().BoolVar(&v, "verbose", false, "verbose output")
+	rootCmd.PersistentFlags().StringVar(&ignoreList, "ignore", "", "comma separated list of interfaces to ignore")
+
+	viper.BindPFlag("out", rootCmd.PersistentFlags().Lookup("out"))
+	viper.BindPFlag("pkgs", rootCmd.PersistentFlags().Lookup("pkgs"))
+	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
+	viper.BindPFlag("ignore", rootCmd.PersistentFlags().Lookup("ignore"))
 }
