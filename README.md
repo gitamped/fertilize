9213daa46964d1d4f8d89b204e778e67ca5bed05
gitamped <126345532+gitamped@users.noreply.github.com>
2023-02-27 19:41:13 -0500

    updates readme. updated default msg

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # fertilize
-A Go reflection tool to extract type information from packages
-
+Fertilize describes Go packages in a generic way.Use output json to generate boilerplate code and documentation with a template engine of your choice.
 
 # Examples
-Run `go build`. Run `./fertilize`
+```
+Usage:
+  fertilize [flags]
+
+Flags:
+  -h, --help            help for fertilize
+      --ignore string   comma separated list of interfaces to ignore
+      --out string      output file (default: stdout)
+      --pkgs string     comma separated list of package patterns (default "./...")
+      --verbose         verbose output
+```
--- cmd/run.go
+++ cmd/run.go
@@ -21,8 +21,10 @@ var (
 var rootCmd = &cobra.Command{
 	Use:   "fertilize",
 	Short: "Fertilize describes Go packages in a generic way.",
-	Long: `A way to generate package information to use in a template engine.
-		   Use to generate boilerplate code and documents.`,
+	Long: `Fertilize describes Go packages in a generic way.
+
+Use the output json to generate boilerplate code and documentation with
+a template engine of your choice.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		patterns := strings.Split(viper.GetString("pkgs"), ",")
@@ -47,7 +49,7 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().StringVar(&outfile, "out", "", "output file (default: stdout)")
-	rootCmd.PersistentFlags().StringVar(&pkgs, "pkgs", "./...", "comma separated list of package patterns (default: ./...).")
+	rootCmd.PersistentFlags().StringVar(&pkgs, "pkgs", "./...", "comma separated list of package patterns")
 	rootCmd.PersistentFlags().BoolVar(&v, "verbose", false, "verbose output")
 	rootCmd.PersistentFlags().StringVar(&ignoreList, "ignore", "", "comma separated list of interfaces to ignore")
 
