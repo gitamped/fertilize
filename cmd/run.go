b661388d772d98b8fd49c7929b9d2874d7055d1a
gitamped <126345532+gitamped@users.noreply.github.com>
2023-07-27 17:23:31 -0400

    fixes exclude interface bug

--- cmd/run.go
+++ cmd/run.go
@@ -3,8 +3,11 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
+	"text/template"
 
 	"github.com/gitamped/fertilize/parser"
 	"github.com/spf13/cobra"
@@ -14,6 +17,7 @@ import (
 var (
 	outfile    string
 	pkgs       string
+	tmplPath   string
 	v          bool
 	ignoreList string
 )
@@ -35,8 +39,26 @@ a template engine of your choice.`,
 		if err != nil {
 			panic("err parsing")
 		}
+
 		b, err := json.Marshal(def)
-		fmt.Printf(string(b))
+		t, _ := os.ReadFile(tmplPath)
+		var data map[string]parser.Definition
+		json.Unmarshal(b, &data)
+
+		tmpl, _ := template.New("test").Parse(string(t))
+
+		for _, v := range data {
+			os.Truncate(filepath.Join(outfile), 0)
+			f, err := os.OpenFile(filepath.Join(outfile), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			tmpl.Execute(f, v)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	},
 }
 
@@ -50,11 +72,13 @@ func Execute() {
 func init() {
 	rootCmd.PersistentFlags().StringVar(&outfile, "out", "", "output file (default: stdout)")
 	rootCmd.PersistentFlags().StringVar(&pkgs, "pkgs", "./...", "comma separated list of package patterns")
+	rootCmd.PersistentFlags().StringVar(&tmplPath, "tmpl", "handler.tmpl", "template filepath")
 	rootCmd.PersistentFlags().BoolVar(&v, "verbose", false, "verbose output (default: false)")
 	rootCmd.PersistentFlags().StringVar(&ignoreList, "ignore", "", "comma separated list of interfaces to ignore")
 
 	viper.BindPFlag("out", rootCmd.PersistentFlags().Lookup("out"))
 	viper.BindPFlag("pkgs", rootCmd.PersistentFlags().Lookup("pkgs"))
+	viper.BindPFlag("tmpl", rootCmd.PersistentFlags().Lookup("tmpl"))
 	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
 	viper.BindPFlag("ignore", rootCmd.PersistentFlags().Lookup("ignore"))
 }
